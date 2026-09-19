728391f1a3e0cc9d20c97fb27e90faa57616d419
rajudasrudrov2 <contactwithrudro@gmail.com>
2026-09-19 19:44:46 +0600

    Remove old project files before RDR-V2-WORK-01 replacement

--- robots.txt.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute } from 'astro';
-export const GET: APIRoute = () => new Response(`User-agent: *
-Allow: /
-Sitemap: https://rajudasrudro.com/sitemap.xml
-`, { headers: { 'Content-Type': 'text/plain' } });
