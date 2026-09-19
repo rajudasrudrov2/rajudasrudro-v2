fc2932389ec9cdf0f078063fc6319b8e63052576
rajudasrudrov2 <contactwithrudro@gmail.com>
2026-09-20 03:46:16 +0600

    Add files via upload

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
   readonly PUBLIC_SITE_ENV?: string;
   readonly PUBLIC_SITE_URL?: string;
-  readonly PUBLIC_CONTACT_ENDPOINT?: string;
   readonly CMS_GRAPHQL_URL?: string;
   readonly CMS_REST_URL?: string;
   readonly CONTACT_DELIVERY_API_KEY?: string;
